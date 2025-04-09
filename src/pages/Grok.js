46cee605afb3c2997d9198330acf4dada945983e
iabhaykaran <iamabhaykaran@gmail.com>
2025-04-10 04:09:43 +0530

    msg

--- src/pages/Grok.js
+++ src/pages/Grok.js
@@ -1,19 +1,33 @@
 export default function Grok() {
   return (
-    <div className="notification-page  page">
-      <div className="page-top-div "></div>
-      <div
-        style={{ marginTop: "250px", textAlign: "center", fontWeight: "500" }}
-      >
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        height: "100vh",
+      }}
+    >
+      <div>
         <h1>Grok something</h1>
-        <p>Premium subscribers can now use our most advanced AI Grok on X.</p>
 
-      <div  style={{marginLeft:"160px",marginTop:"10px",   backgroundColor:"black"}} className="nav-div profile-btn">
-        {/* <NavLink to="/hoe">Profile</NavLink> */}
-        <div style={{  }}>
-          <h3>Subscribe</h3>
-        </div>
+        <p>
+          Grok is a powerful AI assistant that can help you with a variety of tasks, including coding, learning, and problem-solving.
+          
+        </p>
       </div>
+      <br />
+      <div
+        style={{
+          background: "black",
+          borderRadius: "25px",
+          color: "white",
+          padding: "10px  20px",
+        }}
+      >
+        <h3>Subscribe</h3>
       </div>
     </div>
   );
