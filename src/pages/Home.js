7fc9c46934f0ecdf30dbb803d52d1c4fe983b59e
iabhaykaran <iamabhaykaran@gmail.com>
2025-04-17 22:15:37 +0530

    msg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,70 +37,27 @@ export default function Home() {
             uname="@yashmaheshwari"
           />
           <Postcard
-            img="post.jpeg"
+            img="post3.jpg"
             dp="dp.jpg"
             name="Abhay Tiwari"
-            caption=" #BreakingNews The definition of a woman and sex in the Equality Act relates to “a biological woman and biological sex”, the Supreme Court has ruled as it unanimously allowed an appeal from a campaign group  "
             uname="@abhaykaran"
+            caption="Lee Corso, the legendary ESPN broadcaster and former coach who has entertained fans for 38 college football seasons, will make his final headgear pick on College GameDay on Saturday, Aug. 30 – Week 1 of the 2025 college football season."
           />
           <Postcard
-            img="post22.jpeg"
+            img="post2.jpeg"
             dp="dp2.webp"
-            caption="Dear 'trans women',You are men. You are men biologically and in law. You will always be men and you always were men. Now stay out of women's spaces.Best"
+            caption="    Deciline of wesrtern Civilizartion."
             name="Yash Maheshwari"
             uname="@yashmaheshwari"
           />
-          <Postcard
-            img="post.jpeg"
-            dp="dp.jpg"
-            name="Abhay Tiwari"
-            caption=" #BreakingNews The definition of a woman and sex in the Equality Act relates to “a biological woman and biological sex”, the Supreme Court has ruled as it unanimously allowed an appeal from a campaign group  "
-            uname="@abhaykaran"
-          />
-          <Postcard
-            img="post22.jpeg"
-            dp="dp2.webp"
-            caption="Dear 'trans women',You are men. You are men biologically and in law. You will always be men and you always were men. Now stay out of women's spaces.Best"
-            name="Yash Maheshwari"
-            uname="@yashmaheshwari"
-          />
-          <Postcard
-            img="post.jpeg"
-            dp="dp.jpg"
-            name="Abhay Tiwari"
-            caption=" #BreakingNews The definition of a woman and sex in the Equality Act relates to “a biological woman and biological sex”, the Supreme Court has ruled as it unanimously allowed an appeal from a campaign group  "
-            uname="@abhaykaran"
-          />
-          <Postcard
-            img="post22.jpeg"
-            dp="dp2.webp"
-            caption="Dear 'trans women',You are men. You are men biologically and in law. You will always be men and you always were men. Now stay out of women's spaces.Best"
-            name="Yash Maheshwari"
-            uname="@yashmaheshwari"
-          />
-          <Postcard
-            img="post.jpeg"
-            dp="dp.jpg"
-            name="Abhay Tiwari"
-            caption=" #BreakingNews The definition of a woman and sex in the Equality Act relates to “a biological woman and biological sex”, the Supreme Court has ruled as it unanimously allowed an appeal from a campaign group  "
-            uname="@abhaykaran"
-          />
-          <Postcard
-            img="post22.jpeg"
-            dp="dp2.webp"
-            caption="Dear 'trans women',You are men. You are men biologically and in law. You will always be men and you always were men. Now stay out of women's spaces.Best"
-            name="Yash Maheshwari"
-            uname="@yashmaheshwari"
-          />
+         
 
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-
-
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
         </div>
       </div>
       <div style={{ display: "flex", flexDirection: "column" }}>
